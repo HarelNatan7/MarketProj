3a14a7c996f6b0dba8cfb7ba4a0ba8cf5c2c5a69
OrenKotSalomon <orensalomonkot@gmail.com>
2023-02-05 14:47:25 +0200

    changed functions

--- frontend/src/pages/sign-up.jsx
+++ frontend/src/pages/sign-up.jsx
@@ -8,35 +8,6 @@ import { userService } from '../services/user.service.js'
 import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service.js';
 
 export function SignUp() {
-    // const boards = useSelector((storeState) => storeState.boardModule.boards)
-    const [credentials, setCredentials] = useState({ email: '', username: '', password: '', fullname: '', imgUrl: '' })
-    const navigate = useNavigate()
-
-    function clearState() {
-        setCredentials({ email: '', username: '', password: '', fullname: '', imgUrl: '' })
-    }
-
-    function handleChange(ev) {
-        const field = ev.target.name
-        const value = ev.target.value
-        setCredentials({ ...credentials, [field]: value })
-    }
-
-    async function onSignup(ev = null) {
-        if (ev) ev.preventDefault()
-        if (!credentials.email || !credentials.password || !credentials.fullname) return
-        // console.log('credentials:', credentials)
-        try {
-            const user = await userService.signup(credentials)
-            showSuccessMsg(`Welcome ${user.fullname}`)
-            clearState()
-            // navigate(`/board/${boards[0]._id}`)
-        }
-        catch (err) {
-            // showErrorMsg('OOps try again', err)
-            console.log('error: ', err)
-        }
-    }
 
     return (
         <section className='signup'>
@@ -46,7 +17,8 @@ export function SignUp() {
                     {" Up"}
                 </h1>
                 <form className="email-password-input-and-button-container"
-                    onSubmit={onSignup}>
+                >
+                    {/* onSubmit={onSignup} */}
                     <div className="input-container">
                         <label>Name</label>
                         <input type="text" name="name" id="name" placeholder="Enter your name" />
@@ -59,7 +31,7 @@ export function SignUp() {
                         <label className="email-password-label">Email</label>
                         <div className="email-input-container">
                             <input
-                                onChange={handleChange}
+                                // onChange={handleChange}
                                 id="email"
                                 placeholder="Example@company.com"
                                 type="email"
