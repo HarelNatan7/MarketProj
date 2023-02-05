f5db538dd164ac8bf67eaca3dcce2ccdbc425450
Harel Natan <harelnatan7@gmail.com>
2023-02-05 14:33:19 +0200

    Another Clean

--- frontend/src/root-cmp.jsx
+++ frontend/src/root-cmp.jsx
@@ -1,24 +1,17 @@
 import React from 'react'
 import { Routes, Route } from 'react-router'
 
-import routes from './routes'
-
 import { AppHeader } from './cmps/app-header'
-import { AppFooter } from './cmps/app-footer'
-import { UserDetails } from './pages/user-details'
 import { SignUp } from './pages/sign-up'
 
 export function RootCmp() {
 
-    return (
-        <div>
-            <AppHeader />
-            <main>
-                <Routes>
-                    <Route element={<SignUp />} path="/" />
-                </Routes>
-            </main>
-            <AppFooter />
-        </div>
-    )
+    return <div>
+        <AppHeader />
+        <main>
+            <Routes>
+                <Route element={<SignUp />} path="/" />
+            </Routes>
+        </main>
+    </div>
 }
