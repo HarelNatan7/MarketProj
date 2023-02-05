7f6c2b10119ed2bd76fc0d815e0b2dbc25043093
Harel Natan <harelnatan7@gmail.com>
2023-02-05 14:59:50 +0200

    Added Radio and Range inputs

--- frontend/src/pages/sign-up.jsx
+++ frontend/src/pages/sign-up.jsx
@@ -25,15 +25,12 @@ export function SignUp() {
     async function onSignup(ev = null) {
         if (ev) ev.preventDefault()
         if (!credentials.email || !credentials.password || !credentials.fullname) return
-        // console.log('credentials:', credentials)
         try {
             const user = await userService.signup(credentials)
             showSuccessMsg(`Welcome ${user.fullname}`)
             clearState()
-            // navigate(`/board/${boards[0]._id}`)
         }
         catch (err) {
-            // showErrorMsg('OOps try again', err)
             console.log('error: ', err)
         }
     }
@@ -60,10 +57,44 @@ export function SignUp() {
                                 aria-label="Enter your work email address" required />
                         </div>
                     </div>
+                    <div>
+                        <legend>How many years of experience do you
+                            have with Facebook Marketing?</legend>
+
+                        <div>
+                            <input type="radio" id="no-experience" name="exp" value="no-experience" />
+                            <label htmlFor="no-experience">No Experience</label>
+                        </div>
+
+                        <div>
+                            <input type="radio" id="0-1-years" name="exp" value="0-1-years" />
+                            <label htmlFor="0-1-years">0-1 Years</label>
+                        </div>
+
+                        <div>
+                            <input type="radio" id="1-2-years" name="exp" value="1-2-years" />
+                            <label htmlFor="1-2-years">1-2 Years</label>
+                        </div>
+                        <div>
+                            <input type="radio" id="2-or-more" name="exp" value="2-or-more" />
+                            <label htmlFor="2-or-more">2 Or More Years</label>
+                        </div>
+                    </div>
+                    <div className="range-input-container">
+                        <label className="quest-label-label">What was the
+                            biggest campaign budget you have managed in a single month?</label>
+                        <input
+                            onChange={handleChange}
+                            id="range"
+                            type="range"
+                            name="range"
+                            min="1000" max="500000"
+                            className="range-input" />
+                    </div>
 
                     <div className="btn-container">
                         <button type="submit" className="next-btn">
-                            <div className="next-wrapper">Sign up</div>
+                            <div className="next-wrapper">Submit</div>
                         </button>
                     </div>
 
