73ce921012de65bbfca50503b02c85ab8dc160e6
OrenKotSalomon <orensalomonkot@gmail.com>
2023-02-05 15:57:18 +0200

    added styling to users

--- frontend/src/pages/sign-up.jsx
+++ frontend/src/pages/sign-up.jsx
@@ -20,15 +20,15 @@ export function SignUp() {
                 >
                     {/* onSubmit={onSignup} */}
                     <div className="input-container">
-                        <label>Name</label>
+                        <span>Name</span>
                         <input type="text" name="name" id="name" placeholder="Enter your name" />
                     </div>
                     <div className="input-container">
-                        <label>Last Name</label>
+                        <span>Last Name</span>
                         <input type="text" name="lastName" id="lastName" placeholder="Enter your last name" />
                     </div>
                     <div className="input-container">
-                        <label className="email-password-label">Email</label>
+                        <span className="email-password-label">Email</span>
                         <div className="email-input-container">
                             <input
                                 // onChange={handleChange}
@@ -41,40 +41,44 @@ export function SignUp() {
                         </div>
                     </div>
                     <div className="input-container">
-                        <label>Website Address</label>
+                        <span>Website Address</span>
                         <input type="text" name="website" id="website" placeholder="Website Address" />
                     </div>
                     <div className="input-container">
-                        <label>LinkedIn Address
-                        </label>
+                        <span>LinkedIn Address
+                        </span>
                         <input type="text" name="linkdin" id="linkdin" placeholder="Linkdin profile" />
                     </div>
-                    <div>
+                    <div className="radio-input-container">
                         <legend>How many years of experience do you
                             have with Facebook Marketing?</legend>
 
-                        <div>
-                            <input type="radio" id="no-experience" name="exp" value="no-experience" />
-                            <label htmlFor="no-experience">No Experience</label>
-                        </div>
+                        <div className="radio-wrapper">
+                            <div className="radio-container">
 
-                        <div>
-                            <input type="radio" id="0-1-years" name="exp" value="0-1-years" />
-                            <label htmlFor="0-1-years">0-1 Years</label>
-                        </div>
+                                <input type="radio" id="no-experience" name="exp" value="no-experience" />
+                                <span htmlFor="no-experience">No Experience</span>
+                            </div>
 
-                        <div>
-                            <input type="radio" id="1-2-years" name="exp" value="1-2-years" />
-                            <label htmlFor="1-2-years">1-2 Years</label>
-                        </div>
-                        <div>
-                            <input type="radio" id="2-or-more" name="exp" value="2-or-more" />
-                            <label htmlFor="2-or-more">2 Or More Years</label>
+                            <div className="radio-container">
+                                <input type="radio" id="0-1-years" name="exp" value="0-1-years" />
+                                <span htmlFor="0-1-years">0-1 Years</span>
+                            </div >
+
+                            <div className="radio-container">
+                                <input type="radio" id="1-2-years" name="exp" value="1-2-years" />
+                                <span htmlFor="1-2-years">1-2 Years</span>
+                            </div>
+                            <div className="radio-container">
+                                <input type="radio" id="2-or-more" name="exp" value="2-or-more" />
+                                <span htmlFor="2-or-more">2 Or More Years</span>
+                            </div>
                         </div>
                     </div>
+
                     <div className="range-input-container">
-                        <label className="quest-label-label">What was the
-                            biggest campaign budget you have managed in a single month?</label>
+                        <span className="quest-label-label">What was the
+                            biggest campaign budget you have managed in a single month?</span>
                         <input
                             id="range"
                             type="range"
